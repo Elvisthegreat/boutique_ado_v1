3defca139767ebedd78333cdd4b30f97f7c02765
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-05-16 14:45:26 +0000

    Added products views and templates

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')), # that we need for login,logour and so on for django
     path('', include('home.urls')),
+    path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
