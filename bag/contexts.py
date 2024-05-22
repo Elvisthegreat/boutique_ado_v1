8e352aa2771d9368293a9bea35e10ad69a186871
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-05-22 00:45:10 +0000

    Corrected error distracting template from working

--- bag/contexts.py
+++ bag/contexts.py
@@ -20,19 +20,16 @@ def bag_contents(request):
             'product': product,
         })
 
-    if total < settings.FREE_DELIVERY_THRESHOLD: # free delivery. if they spend more than the amount specified in the free delivery threshold in settings.py.
+    if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total #  let the user know how much more they need to spend to get free delivery
-    else: # If the total is greater than or equal to the threshold set delivery and the free_delivery_delta to 0.
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
         delivery = 0
         free_delivery_delta = 0
-
+    
     grand_total = delivery + total
-
+    
     context = {
-        """add all these items to the context.
-        So they'll be available in templates across the site."""
-        
         'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
