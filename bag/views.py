5d94c9c1a604aa3c86373acf313ed11861595d16
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-05-22 16:20:28 +0000

    Handling the shooping bag

--- bag/views.py
+++ bag/views.py
@@ -11,12 +11,30 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity')) # get the quantity from the form. and convert it to an integer since it'll come from the template as a string.
     redirect_url = request.POST.get('redirect_url') # redirect_url found in our form
+
+    """ if product size is in request.post we'll set it equal to that. """
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['size']
     bag = request.session.get('bag', {}) # first check to see if there's a bag variable in the session. And if not we'll create one.
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity # Or update the quantity if it already exists.
+    if size:
+        """If the item is already in the bag.
+        Then we need to check if another item of the same id and same size already exists.
+        And if so increment the quantity for that size and otherwise just set it equal to the quantity"""
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}} # To handle maybe same item with different sizes when we add it to the bag
     else:
-        bag[item_id] = quantity # Add the item to the bag
+
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity # Or update the quantity if it already exists.
+        else:
+            bag[item_id] = quantity # Add the item to the bag
 
     request.session['bag'] = bag # And then overwrite the variable in the session with the updated version.
     return redirect(redirect_url)
