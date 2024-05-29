506f1b62f1d1f356ca0d7aa371001694e826a384
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-05-29 13:53:34 +0000

    Added checkout views and template

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -83,6 +83,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media', # if a product doesn't have an image.
                 'bag.contexts.bag_contents',
             ],
             'builtins': [
