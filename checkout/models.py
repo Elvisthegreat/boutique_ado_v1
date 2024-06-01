a807385df8df74cb7147b65c9f5b699871792b33
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-06-01 15:23:52 +0000

    Added checkout succes logic

--- checkout/models.py
+++ checkout/models.py
@@ -6,8 +6,6 @@ from django.conf import settings
 
 from products.models import Product
 
-# Create your models here.
-
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
@@ -36,13 +34,13 @@ class Order(models.Model):
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
             self.delivery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
-        self.save() 
+        self.save()
 
     def save(self, *args, **kwargs):
         """
@@ -60,10 +58,9 @@ class Order(models.Model):
 class OrderLineItem(models.Model):
     """ The basic idea here. Is when a user checks out.
         We'll first use the information they put into the payment form to create an order instance. """
-
-    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitem')
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL,
+    product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
@@ -76,4 +73,4 @@ class OrderLineItem(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'SKU {self.product.sku} or order {self.order.order_number}'
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
