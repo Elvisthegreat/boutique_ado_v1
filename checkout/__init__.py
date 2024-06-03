1f642cb3d7cd0fc20c1eae84877b8d7210680fb2
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-06-03 15:45:32 +0000

    Handling the webhook handler

--- checkout/__init__.py
+++ checkout/__init__.py
@@ -1,1 +1,7 @@
+"""
+__init__.py : Sets the default app configuration.
+apps.py: Defines the app configuration and imports signals when the app is ready.
+signals.py: Contains signal handlers that update the order total whenever an OrderLineItem is saved or deleted.
+"""
+
 default_app_config = 'checkout.apps.CheckoutConfig'
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,4 +1,4 @@
-from dgango.http import HttpResponse
+from django.http import HttpResponse
 
 class StripeWH_Handler:
     """Handle stripe webhooks"""
@@ -8,9 +8,25 @@ class StripeWH_Handler:
 
     def handle_event(self, event):
         """
-        Handle a generic/unknown/unexpected webhook event
+        Handle a generic/unknown/unexpected webhook event.
+        return an HTTP response indicating it was received.
         """
+        return HttpResponse(
+            content=f'Unhandled Webhook received: {event["type"]}',
+            status=200)
 
+    def handle_payment_intent_succeded(self, event):
+        """
+        Handle the payment_intent.succeded webhook from stripe
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle the payment_intent.payment_failed webhook from stripe
+        """
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
