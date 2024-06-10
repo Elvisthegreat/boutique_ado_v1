c84f454be10286cd99846462706353197e9e05d2
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-06-10 17:31:14 +0000

    Profiles part 4 - added order history and updated checkout views

--- checkout/views.py
+++ checkout/views.py
@@ -6,6 +6,8 @@ from django.conf import settings
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
 from bag.contexts import bag_contents
 
 import stripe # imported stripe
@@ -113,8 +115,28 @@ def checkout(request):
             amount=stripe_total,
             currency=settings.STRIPE_CURRENCY,
         )
+        
+        # Since user now have profile user, users default delivery information to pre-fill the form on checkout page
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(), # For example, we can fill in the full_name with the built in get_full_name method on their user account to pre-fill the fields
+                    'email': profile.user.email, # Their email from their user account. And everything else from the default information in their profile.
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'county': profile.default_county,
+                })
+            except UserProfile.DoesNotExist: # If the user does not exist
+                order_form = OrderForm()
+        # Else render an empty form
+        else:
 
-        order_form = OrderForm()
+            order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
@@ -140,6 +162,30 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get('save_info') # first check whether the user wanted to save their information by getting that from the session just like we get the shopping bag.
     order = get_object_or_404(Order, order_number=order_number)
+     
+     # Only if user is authenticated
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user) # We've already got the order so to add the profile we just have to check if the user is authenticated because if so they'll have a profile that was created when they created their account.
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
+
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+                #  End if statement
+
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
--- profiles/views.py
+++ profiles/views.py
@@ -3,11 +3,13 @@ from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
+
+from checkout.models import Order
 # Create your views here.
 
 def profile(request):
     """ Display the user's profile """
-    profile = get_object_or_404(UserProfile, user=request.user)
+    profile = get_object_or_404(UserProfile, user=request.user) # Storing the user models in a variable name profile
 
     if request.method == 'POST': # If the request method is Post from the user
         form = UserProfileForm(request.POST, instance=profile) # Create a new instance of the user profile form using the post data.
@@ -23,7 +25,23 @@ def profile(request):
     context = {
         'form': form,
         'orders': orders,
-        'on_profile_page': True
+        'on_profile_page': True 
+    }
+
+    return render(request, template, context)
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html' #  use the checkout success template since that template already has the layout for rendering a nice order confirmation.
+    context = {
+        'order': order,
+        'from_profile': True, # So we can check in that template if the user got there via the order history view. Check button of checkout_success page
     }
 
     return render(request, template, context)
