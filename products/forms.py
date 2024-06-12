3a7c1ac42170369abb212938a8835943319f123e
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-06-12 11:44:56 +0000

    Product admin - and product view, url template

--- products/forms.py
+++ products/forms.py
@@ -4,7 +4,7 @@ from .models import Product, Category
 
 class ProductForm(forms.ModelForm):
 
-    class Meta:
+    class Meta: # Defined the model and the field we want to include
         model = Product
         fields = '__all__'
         
