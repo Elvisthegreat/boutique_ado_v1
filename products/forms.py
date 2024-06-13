52c599f40dc15a4c8a4df814ea4189a682564166
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-06-13 17:04:20 +0000

    Products Admin - Fixing the image field part 1

--- products/forms.py
+++ products/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput # class imported from the widgets in our directory
 from .models import Product, Category
 
 
@@ -7,12 +8,14 @@ class ProductForm(forms.ModelForm):
     class Meta: # Defined the model and the field we want to include
         model = Product
         fields = '__all__'
-        
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            categories = Category.objects.all()
-            friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
-            self.fields['category'].choices = friendly_names
-            for field_name, field in self.fields.items():
-                field.widget.attrs['class'] = 'border-black rounded-0'
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
