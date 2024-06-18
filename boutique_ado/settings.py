b64db818bc85b4ea7694cb07669f5e1f07974e51
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-06-18 21:28:48 +0000

    Corrected error in env.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -18,8 +18,6 @@ import dj_database_url
 if os.path.exists("env.py"):
   import env
 
-from env import SECRET_KEY
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
