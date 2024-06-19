ad4b3198d58fa4fd896d7e4f196504ab110e357b
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-06-19 18:05:46 +0000

    env.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-elvisthegre-boutiqueado-pztiygkg456.ws-eu114.gitpod.io', 'boutique-ado-walkthrougproject-4fa88fb1c5cb.herokuapp.com', 'localhost']
 
